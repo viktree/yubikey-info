a42f4a05fb04ebf217bcfee7b6f62d6aa249baa5
Vikram Venkataranan <vikram.venkataramanan@mail.utoronto.ca>
2021-06-24 21:05:50 -0400

    add integration test folder
    More Details:

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,25 @@ fn reverse<T: Clone>(xs: &[T]) -> Vec<T> {
 	rev
 }
 
-fn add(x: i32, y: i32) -> i32 {
+pub fn add(x: i32, y: i32) -> i32 {
 	return x + y;
+}
+
+fn sqrt(number: f64) -> Result<f64, String> {
+	if number >= 0.0 {
+		Ok(number.powf(0.5))
+	} else {
+		Err("negative floats don't have square roots".to_owned())
+	}
 }
 
 fn main() {
 	add(1, 2);
 	let lst = [1, 2, 3];
 	reverse(&lst);
+	let res = sqrt(9.0);
 	println!("Hello World!");
+	println!("{:?}", res);
 }
 
 #[cfg(test)]
@@ -26,6 +36,14 @@ mod test {
 	#[test]
 	fn test_add() {
 		assert_eq!(add(1, 2), 3);
+	}
+
+	#[test]
+	#[ignore]
+	fn test_sqrt() -> Result<(), String> {
+		let x = 4.0;
+		assert_eq!(sqrt(x)?.powf(2.0), x);
+		Ok(())
 	}
 
 	#[quickcheck]
